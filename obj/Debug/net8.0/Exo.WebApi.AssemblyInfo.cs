b09d0887336eb5191d3037007e0a104992a7d120
Bre_S2 <brenoestevaopbb@hotmail.com>
2024-09-02 22:43:17 -0300

    atividade 020924

--- obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9177688ae96fb5d2f486c7c7558d7de52a3be1b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
